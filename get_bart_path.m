dad8a60ead1aacac326b687e5a8f27e3ac6521e1
Aman Sharma <amansharma1772@gmail.com>
2024-04-26 00:51:41 -0700

    suppressed output from intermediate variables

--- get_bart_path.m
+++ get_bart_path.m
@@ -7,9 +7,9 @@ function [bart_path, isWSL] = get_bart_path()
 % 2022 Jon Tamir <jtamir.utexas.edu>
 	
 	isWSL = false % assumes sockeye - linux cluster  
-	[bart_installed, version_installed]	= system('bart version')
+	[bart_installed, version_installed]	= system('bart version');
 	if bart_installed == 0
-		bart_path = version_installed
+		bart_path = version_installed;
 	% Check bart toolbox path
 	% bart_path = getenv('BART_TOOLBOX_PATH');
 	% isWSL = false;
